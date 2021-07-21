169abde6c3d5112820c68cc101e615b988b7632b
Ransingh88 <rajaransingh@gmail.com>
2021-07-21 16:49:35 +0530

    nav bar styled component updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,73 @@
 import styled from 'styled-components'
-import styles from './Navbar.module.css'
+// import styles from './Navbar.module.css'
 export function Navbar() {
+
+    const Navbar = styled.div`
+    
+    *{
+    padding: 0;
+    margin: 0;
+    
+    font-family: 'Hind Vadodara', sans-serif;
+    font-weight: 400;
+}
+
+box-sizing: border-box;
+    /* background-color: ${(props) => props.bg}; */
+    width: 100vw;
+    background-image: linear-gradient(to right, #54b98f , #1b8c78);
+    padding: 0.5rem 10rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+div:nth-child(1){
+    /* background-color: #ccc; */
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    padding: 0.4rem 0.6rem;
+    width: 25rem;
+}
+div:nth-child(1) img{
+    width: 10rem;
+}
+div:nth-child(1) input{
+    background-color: transparent;
+    border-radius: 1.5em;
+    outline: none;
+    border: 1px solid ${(props) => props.clr};
+    height: 2.3em;
+    /* width: 10em; */
+}
+ div:nth-child(1) input::placeholder{
+    color: ${(props) => props.clr};
+    padding-left: 1.5em;
+    font-size: 0.8rem;
+    font-weight: 400;
+}
+div:nth-child(2){
+    /* background-color: #ccc; */
+
+}
+div:nth-child(2) ul{
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+}
+div:nth-child(2) ul li{
+    margin: 0 0.8em;
+    list-style-type: none;
+    color: ${(props) => props.clr};
+    font-weight: 500;
+}
+    
+    
+    `
+
     return (
-        <div className={styles.container}>
+        // <div className={styles.container}>
+        <Navbar bg="#eee" clr="#eee">
             <div>
                 <img src="/images/impactguru-white.png" alt="dggdfg" />
                 <input type="search" placeholder="Search"/>
@@ -17,6 +82,6 @@ export function Navbar() {
                     <li>Price</li>
                 </ul>
             </div>
-        </div>
+        </Navbar>
     )
 }
