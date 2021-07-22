a5878ee6dc1f9313ac637ead8c7078f9fb455357
Ransingh88 <rajaransingh@gmail.com>
2021-07-22 23:29:22 +0530

    updated

--- src/components/Catagory/Catagory.jsx
+++ src/components/Catagory/Catagory.jsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper.min.css";
-import "swiper/components/navigation/navigation.min.css";
+import "swiper/components/navigation/navigation.min.css"
 import SwiperCore, { Autoplay, Pagination, Navigation } from "swiper/core";
 
 // install Swiper modules
@@ -22,12 +22,21 @@ export function Catagory() {
       /* margin: 30px 0; */
       padding: 20px 6px;
       /* background-color: red; */
+      
     }
-    .swiper-container {
-      padding: 30px 50px;
-      /* position: absolute; */
+    .swiper-container{
+        padding: 30px 50px;
+        /* position: absolute; */
+        
+        
     }
+
   `;
+    
+
+    
+    
+    
   return (
     <CatagoryContainer>
       <LabelHeading color="green">Crowdfunding Categories</LabelHeading>
@@ -35,8 +44,8 @@ export function Catagory() {
         {/* <CatagoryCard title="Emergency" icon="/icons/ambulance.png" /> */}
         <Swiper
           spaceBetween={30}
-          centeredSlides={true}
-          speed={4000}
+                  centeredSlides={true}
+                  speed={4000}
           autoplay={{
             delay: 2500,
             disableOnInteraction: false,
@@ -44,9 +53,10 @@ export function Catagory() {
           pagination={{
             clickable: true,
           }}
-          navigation={true}
-          slidesPerView={5}
-          loop={true}
+                  navigation={true}
+                  slidesPerView={5} loop={true}
+                  
+                  
         >
           <SwiperSlide>
             <CatagoryCard title="Emergency" icon="/icons/ambulance.png" />
