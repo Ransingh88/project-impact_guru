7d68d4643773f4e3e9e886d23953c0e9e01141d8
ranjan1o <84707304+ranjan1o@users.noreply.github.com>
2021-07-23 12:45:33 +0530

    Add files via upload

---
+++ src/components/HowItWork/carousel/Carousel1.jsx
@@ -0,0 +1,55 @@
+import styled from "styled-components"
+
+
+
+export function Slide4() {
+
+    const Set12 = styled.div`{
+   
+     img{
+      
+     }
+     img:nth-child(1){
+         color:grey;
+         padding:10px;
+          -webkit-filter: grayscale(100%); /* Safari 6.0 - 9.0 */
+  filter: grayscale(100%);
+  width: 28%;
+}
+    img:nth-child(2){
+   
+    width: 35%;
+}
+    img:nth-child(3){
+        padding:10px;
+         
+         -webkit-filter: grayscale(100%); /* Safari 6.0 - 9.0 */
+  filter: grayscale(100%);
+     width: 28%;
+}
+.crousel{
+    width:20%;
+    display:flex;
+  padding:2rem;
+    text-align:center;
+font-size: 4rem;
+margin:auto;
+
+}
+}`
+    return <>
+        <Set12>
+            <div style={{ marginTop: "30px", }}>
+            <img src="https://d2aq6dqxahe4ka.cloudfront.net/themes/front/page/images/hiw/Dhairyaraj.png" alt="" />
+            <img src="https://d2aq6dqxahe4ka.cloudfront.net/themes/front/page/images/hiw/Teera.png" alt="" />
+            <img src="https://d2aq6dqxahe4ka.cloudfront.net/themes/front/page/images/hiw/Shubhangi.png" alt="" />
+            </div>
+            <div className="crousel">
+                <div style={{color:"grey"}}>•</div>
+                <div style={{ color:"green" }}>•</div>
+                <div style={{color:"grey"}}>•</div>
+            </div>
+           
+            </Set12>
+        </>
+}
