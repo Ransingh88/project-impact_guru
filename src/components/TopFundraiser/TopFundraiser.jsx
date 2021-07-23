3fbf55ec9d7c8f1350f3aea56d59edac1f72b109
Ransingh88 <rajaransingh@gmail.com>
2021-07-24 01:26:16 +0530

    top fund Raiser Component created

--- src/components/TopFundraiser/TopFundraiser.jsx
+++ src/components/TopFundraiser/TopFundraiser.jsx
@@ -1,7 +1,70 @@
-
-
+import { CardItem } from '../SuccessfulFundraiser/CardItem'
+import styled from "styled-components";
+import Carousel from "react-elastic-carousel";
+import { LabelHeading } from '../Common/LabelHeading'
+import {Button} from '../Common/Button'
+ 
 export function TopFundraiser() {
-    return (
-        
+
+
+    const Main = styled.div`
+    background-color: whitesmoke;
+    `
+    const CarouselContainer = styled.div`
+    /* background-color: whitesmoke; */
+    padding: 2em 2em;
+    width: 90%;
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    Button{
+      margin-top: 50px;
+    }
+    .TopButtons{
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+    }
+    `
+
+    const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 550, itemsToShow: 3, itemsToScroll: 2, pagination: false },
+    { width: 850, itemsToShow: 4 },
+    { width: 1150, itemsToShow: 4, itemsToScroll: 2 },
+    { width: 1450, itemsToShow: 5 },
+    { width: 1750, itemsToShow: 6 },
+  ];
+  return (
+      <Main >
+        <CarouselContainer>
+            <LabelHeading color="green">Our Top Fundraisers</LabelHeading>
+        <Carousel
+          itemsToShow={4}
+          enableAutoPlay
+          autoPlaySpeed={3000}
+          pagination={false}
+          easing="ease"
+          transitionMs={3000}
+          breakPoints={breakPoints}
+        >
+            <CardItem/>
+            <CardItem/>
+            <CardItem/>
+            <CardItem/>
+            <CardItem/>
+            <CardItem/>
+            <CardItem/>
+          </Carousel>
+          <div className="TopButtons">
+              <Button>START A FREE FUNDRAISER</Button>
+              <h5>OR</h5>
+        <Button>START A FREE FUNDRAISER</Button>
+        </div>
+          </CarouselContainer>
+          </Main>
+
     )
 }
