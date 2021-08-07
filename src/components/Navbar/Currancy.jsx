ab4391f722d0ac5003b811b8e25371ccffdf030b
Ransingh88 <rajaransingh@gmail.com>
2021-08-08 03:00:11 +0530

    all done with responsiveness

--- src/components/Navbar/Currancy.jsx
+++ src/components/Navbar/Currancy.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import InputLabel from '@material-ui/core/InputLabel';
-import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
 import NativeSelect from '@material-ui/core/NativeSelect';
 
 const useStyles = makeStyles((theme) => ({
@@ -18,18 +15,18 @@ const useStyles = makeStyles((theme) => ({
 
 export function Currency() {
   const classes = useStyles();
-  const [state, setState] = React.useState({
-    age: '',
-    name: 'hai',
-  });
+  // const [state, setState] = React.useState({
+  //   age: '',
+  //   name: 'hai',
+  // });
 
-  const handleChange = (event) => {
-    const name = event.target.name;
-    setState({
-      ...state,
-      [name]: event.target.value,
-    });
-  };
+  // const handleChange = (event) => {
+  //   const name = event.target.name;
+  //   setState({
+  //     ...state,
+  //     [name]: event.target.value,
+  //   });
+  // };
 
   return (
     <div>
